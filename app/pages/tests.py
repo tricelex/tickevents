bcff841341c9a41b7fa620a82d67a3d0d8039b97
Chuckz Okoye <tricelex@gmail.com>
2020-12-10 17:45:18 +0100

    Fixed flake8 errors

--- app/pages/tests.py
+++ app/pages/tests.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
 
+
 # Create your tests here.
 def app(x, y):
-    return  x + y
+    return x + y
 
 
 class PagesTests(TestCase):
     def test_add_numbers(self):
-        self.assertEqual(app(3,8), 11)
+        self.assertEqual(app(3, 8), 11)
